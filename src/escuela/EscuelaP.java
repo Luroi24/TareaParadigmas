3d311ded6801d6a7b05598d0091ca1c59e587b3a
David-HernandezM <67880616+David-HernandezM@users.noreply.github.com>
2021-11-07 16:17:03 -0600

    Add files via upload

---
+++ src/escuela/EscuelaP.java
@@ -0,0 +1,35 @@
+package SchoolHm.Paradigmas.tareas;
+
+import java.util.ArrayList;
+
+public class EscuelaP {
+    private String name;
+    private ArrayList<Alumno> alumnos;
+
+    public EscuelaP() {
+        this(null);
+    }
+
+    public EscuelaP(String name) {
+        this.name  = name;
+        alumnos = new ArrayList<>();
+    }
+
+    public void addStudent(Alumno alumno) {
+        alumnos.add(alumno);
+    }
+
+    public void imprimirAlumnos() {
+        System.out.println("Alumnos: ");
+        for (Alumno alumno : alumnos) {
+            System.out.println("- Nombre" + alumno.regresaNombre() + "\n" +
+                               "  Matricula: " + alumno.regresaMatricula());
+            alumno.imprimeMaterias();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
