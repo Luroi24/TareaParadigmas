643169f1642bdb71ea0ff2960255f1a9b616deb1
David-HernandezM <67880616+David-HernandezM@users.noreply.github.com>
2021-11-07 16:21:33 -0600

    Update UserEscuela.java

--- src/escuela/UserEscuela.java
+++ src/escuela/UserEscuela.java
@@ -3,6 +3,7 @@ package escuela;
 public class UserEscuela {
 
     public static void main(String[] args) {
+        /*
        Alumno alumno1 = new Alumno("Pepe","1000");
        Materia BD = new Materia("Bases de Datos",3);
        Materia Paradigmas = new Materia("Paradigmas",3);
@@ -11,6 +12,30 @@ public class UserEscuela {
        
         System.out.println("Materias del alumno1");
         alumno1.imprimeMaterias();
+        */
+        EscuelaP escuela = new EscuelaP("ESCOM");
+        Alumno alumno1 = new Alumno("Pepe","1000");
+        Alumno alumno2 = new Alumno("Luis", "1032");
+        Alumno alumno3 = new Alumno("Jose", "4234");
+
+        alumno1.addMateria("Paradigmas", 3);
+        alumno1.addMateria("Base de datos", 3);
+        alumno1.addMateria("Sistemas Operativos", 4);
+
+        alumno2.addMateria("Ec. Diferenciales", 3);
+        alumno2.addMateria("Finanzas empresariales", 3);
+        alumno2.addMateria("Analisis y diseño de algoritmos", 3);
+
+        alumno3.addMateria("Calculo aplicado", 2);
+        alumno3.addMateria("Analisis vectorial", 1);
+        alumno3.addMateria("Algebra lineal", 2);
+
+        escuela.addStudent(alumno1);
+        escuela.addStudent(alumno2);
+        escuela.addStudent(alumno3);
+
+        System.out.println(escuela.getName());
+        escuela.imprimirAlumnos();
         
     }
     
